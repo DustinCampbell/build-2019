a95c02f1a802fc7c75ec3db4d51c589e403cc92d
Dustin Campbell <dustin@teamcampbell.org>
2019-05-06 08:31:24 -0700

    Initial project

---
+++ TollCalculator/TollCalculator.cs
@@ -0,0 +1,29 @@
+﻿using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+using System;
+
+namespace TollCollector
+{
+    public class TollCalculator
+    {
+        public decimal CalculateToll(object vehicle)
+        {
+            switch (vehicle)
+            {
+                case Car _:
+                    return 2.00m;
+                case Taxi _:
+                    return 3.00m;
+                case Bus _:
+                    return 5.00m;
+                case DeliveryTruck _:
+                    return 10.00m;
+                case null:
+                    throw new ArgumentNullException(nameof(vehicle));
+                default:
+                    throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle));
+            }
+        }
+    }
+}
